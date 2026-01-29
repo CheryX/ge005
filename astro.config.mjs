c8a980a7e03b7ca3edfee9d7f34bc164eb038b46
Monika Aki <monika.aki@proton.me>
2026-01-29 12:50:21 +0100

    not ons server anymore

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://wrs.akinom.dev",
-	output: 'server',
+	// output: 'server',
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
